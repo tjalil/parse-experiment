f2238fe82e1926be41db48abdd36ad25dc713ece
Taha Jalil <taha.jalil@gmail.com>
2014-05-30 17:29:46 -0400

    added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username, :first_name, :last_name, :role, :organization_name, :type_of_artist)
+  end
+
+end
