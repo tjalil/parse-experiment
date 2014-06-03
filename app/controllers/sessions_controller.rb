1b53b9805f7b10116653c82799e5cef9270ed109
Taha Jalil <taha.jalil@gmail.com>
2014-06-03 11:27:55 -0400

    created sessions controller to handle login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.authenticate(params[:username], params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged Out!"
   end
-  
+
 end
