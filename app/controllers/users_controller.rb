7a07d8cc48817def33385c2918bf8313f8c55d58
Taha Jalil <taha.jalil@gmail.com>
2014-06-02 21:24:10 -0400

    added uniqueness_of_email validation to user model, edited html and removed extra space

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
     else
       render :new
     end
-
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ParseUser
   fields :email, :password, :username, :first_name, :last_name, :role, :organization_name, :type_of_artist
+
+  validates_uniqueness_of :email
 end
