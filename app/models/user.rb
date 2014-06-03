9346868077f34e5fd0442bfdcf428a335d79b56f
Taha Jalil <taha.jalil@gmail.com>
2014-06-03 11:32:45 -0400

    added validations to user model, moved signup link from index to application.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ParseUser
   fields :email, :password, :username, :first_name, :last_name, :role, :organization_name, :type_of_artist
 
-  validates_uniqueness_of :email
+  validates_presence_of :email, :username, :first_name, :last_name, :password
 end
