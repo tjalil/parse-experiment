1c5c62b55a19f79640bbc054bf3ff3fd32db8aea
Taha Jalil <taha.jalil@gmail.com>
2014-06-04 14:27:44 -0400

    added file type to field :avatar in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ParseUser
   fields :email, :password, :username, :first_name, :last_name, :role, :organization_name, :type_of_artist
 
+  field :avatar, type: File
+
   validates_presence_of :email, :username, :first_name, :last_name, :password
+
 end
