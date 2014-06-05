089ec31289056d46915d670ce78eb63fc8a823be
Taha Jalil <taha.jalil@gmail.com>
2014-06-05 14:49:38 -0400

    created user mailer, defined welcome_email method

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  default from: "folks@parseexperiment.com"
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://localhost:3000/sessions/new'
+    mail(to: @user.email, subject: 'Testing Mailer for parse-experiment')
+  end
+  
+end
