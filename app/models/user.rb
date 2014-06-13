7c0dfaf35a1569c0dd08d520c50cfde164af9bc2
Taha Jalil <taha.jalil@gmail.com>
2014-06-13 19:20:06 -0400

    removed unnecessary fields from user table, added association with teams

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ParseUser
-  fields :email, :password, :username, :first_name, :last_name, :role, :organization_name, :type_of_artist
 
+  fields :email, :password, :username, :first_name, :last_name
   field :avatar, type: File
 
   validates_presence_of :email, :username, :first_name, :last_name, :password
+
+  has_many :teams, as: :owner, inverse_of: :owner
 
 end
